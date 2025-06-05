6289be0c81245445a4e9ee35d2cb1d98b4f1cf9c
moikkaeel <86324551+moikkaeel@users.noreply.github.com>
2025-06-05 23:30:15 +0300

    Can delete notes and change the note image

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
 const addButton = document.querySelector('.add');
 const emptyFeed = document.querySelector('.empty');
-const noteText = document.querySelector('.text');
 const noteContainer = document.getElementById('container');
 
 function addNote() {
+
     const template = document.getElementById('template');
     const clone = template.content.cloneNode(true);
+    const noteImg = clone.getElementById('image');
+    const uploadDiv = clone.querySelector('.upload-photo');
+    const newImg = clone.getElementById('input-file');
+    const deleteBtn = clone.querySelector('.icon');
+
+    // Create onclick events for clones
+
+    noteImg.onclick = () => {
+        uploadDiv.style.display = 'block';
+    }
+
+    // By clicking the delete button the note gets deleted
+    deleteBtn.onclick = () => {
+        // Finds the parent note and gets its id
+        const note = deleteBtn.closest('.note');
+        noteContainer.removeChild(note);
+    }
+
+    // Change note image
+    newImg.onchange = () => {
+        noteImg.src = URL.createObjectURL(newImg.files[0]);
+        console.log("Picture changed");
+
+        // Remove the uploadDiv after changing the picture
+        uploadDiv.style.display = 'none';
+    }
+
+    // Listens for global click and makes upload div disappear if needed
+    document.addEventListener('click', (e) => {
+
+        if(!uploadDiv.contains(e.target) && !noteImg.contains(e.target)) {
+
+            uploadDiv.style.display = 'none';
+        }
+    
+    });
+
+    // Add note to noteContainer
     noteContainer.appendChild(clone);
 };
 
 addButton.addEventListener('click', (e) => {
-
-    console.log('Button pressed');
-
+    
     // Hide the empty feed text
     emptyFeed.style.display = 'none';
-    //
+
     // Add a new note
     addNote();
-
-    console.log(noteContainer.children.length);
-
 });
-
